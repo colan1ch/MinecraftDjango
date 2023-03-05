6b648c1b36b779e0d158bdf6df971a9be5972123
Николай Чернев <nachernev@gmail.com>
2023-03-05 12:58:20 +0300

    зазработка header

--- project/urls.py
+++ project/urls.py
@@ -4,11 +4,11 @@ from main.views import admin_views, main_views, profile_views, authorization_vie
 
 urlpatterns = [
     path('', main_views.index_page, name="Главная"),
-    path('profile', profile_views.profile_page, name="Профиль"),
+    path('profile', profile_views.profile_page, name="profile"),
     path('profile/<int:id>', profile_views.profile_page, name="Чей-то профиль"),
-    path('edit_profile', profile_views.editing_profile_page, name = "Редактирование профиля"),
-    path('registration', authorization_views.registration_page, name = "Регистрация"),
-    path('authorization', authorization_views.login_page, name = "Логин"),
-    path('error404', main_views.error404_page, name = "Ошибка 404"),
+    path('edit_profile', profile_views.editing_profile_page, name="Редактирование профиля"),
+    path('registration', authorization_views.registration_page, name="registration"),
+    path('authorization', authorization_views.login_page, name="login"),
+    path('error404', main_views.error404_page, name="Ошибка 404"),
     path('admin', admin_views.index_page, name="Админ. панель"),
 ]
