82c1a9fd994021b24189ea07d6b11fad8dd8f0cc
Гришанов Никита <nikitka.grishanov@gmail.com>
2023-03-05 12:06:46 +0300

    views refactor

--- main/views/editing_profile_views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from main.views.common import get_context
-
-
-def editing_profile_page(request):
-    context = get_context(request)
-    return render(request, "editing_profile.html", context)
--- main/views/profile_views.py
+++ main/views/profile_views.py
@@ -5,3 +5,8 @@ from main.views.common import get_context
 def profile_page(request):
     context = get_context(request)
     return render(request, "profile.html", context)
+
+
+def editing_profile_page(request):
+    context = get_context(request)
+    return render(request, "editing_profile.html", context)
