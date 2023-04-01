6bd1e91b7418f80a2a886c95285d8f587dfc832b
Тимофей Шишин <89671096060@mail.ru>
2023-04-01 14:31:28 +0300

    Изменение размера поля docker_id в модели Servers с 30 на 64

--- main/models.py
+++ main/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth import get_user_model
 class Servers():
     name = models.CharField(max_length=100)
     user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
-    docker_id = models.CharField(max_length=30)
+    docker_id = models.CharField(max_length=64)
 class User_settings():
     user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
