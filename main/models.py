35063936b4bfb6616bd2884db65aef55d28b05b8
Тимофей Шишин <89671096060@mail.ru>
2023-04-01 14:29:54 +0300

    Изменение типа поля docker_id в модели Servers на charfield

--- main/models.py
+++ main/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth import get_user_model
 class Servers():
     name = models.CharField(max_length=100)
     user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
-    docker_id = models.IntegerField(unique=True)
+    docker_id = models.CharField(max_length=30)
 class User_settings():
     user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
