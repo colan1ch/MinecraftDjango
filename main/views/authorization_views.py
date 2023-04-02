37a93e066889793b3425342bdc3ada6314f91d3d
Fedor__Zorikov <fedor.z1234@yandex.ru>
2023-04-02 10:44:41 +0300

    Создал backend для логина и регистрации.

--- main/views/authorization_views.py
+++ main/views/authorization_views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from main.views.common import get_context
 
 
 def login_page(request):
-    context = get_context(request)
-    return render(request, "authorization.html", context)
+    if request.method == "POST":
+        f = request.POST
+        u = authenticate(username=f.data['login'], password=f.data['psw'])
+        if u is not None:
+            user = User.objects.get(username=f.data['email'])
+            login(request, user)
+            return redirect('/profile')
+        else:
+            return redirect('/authorization')
+    else:
+        if auth.get_user(request).is_authenticated:
+            return redirect('/profile')
+        context = get_context(request)
+        return render(request, "authorization.html", context)
+
 
 def registration_page(request):
-    context = get_context(request)
-    return render(request, "registration.html", context)
+    if request.method == "POST":
+        f = request.POST
+        data = f.data
+        user = User(username=data['login'], email=data['email'], first_name=data['first'], last_name=data['last'])
+        user.set_password(data['psw'])
+        user.save()
+        return redirect('/authorization')
+    else:
+        context = get_context(request)
+        return render(request, "registration.html", context)
