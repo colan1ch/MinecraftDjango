acad7291fe71e08152c4767e05ac64b24d25f905
Тимофей Шишин <89671096060@mail.ru>
2023-04-02 10:32:43 +0300

    Починил миграции джанго

--- main/models.py
+++ main/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
-class Servers():
+class Servers(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
     docker_id = models.CharField(max_length=64)
-class User_settings():
+
+
+class UserSettings(models.Model):
     user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
