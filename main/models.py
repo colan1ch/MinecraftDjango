5094a695b610fcac3940c6c2aacf27b1ff762490
Artem <eartem66@gmail.com>
2023-04-02 11:01:52 +0300

    dedelal

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Page_proj(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=511)
+    online = models.IntegerField()
+    url = models.CharField(max_length=15)
+    img = models.ImageField(upload_to='uploads/', height_field=None, width_field=None, max_length=100)
