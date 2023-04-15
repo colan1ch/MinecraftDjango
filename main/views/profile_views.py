863deecb537ca483dd5dd46c6ddfd2c5e9575b12
GreysTheFox <chernykh.yarik@gmail.com>
2023-04-16 00:20:41 +0300

    Бэк для редактирования профиля(git statusБудет дорабатыватьсяgit status)

--- main/views/profile_views.py
+++ main/views/profile_views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from main.views.common import get_context
+from django.contrib.auth.models import User
 
 
 def profile_page(request):
     context = get_context(request)
     return render(request, "profile.html", context)
 
+def profile_edit_password(request):
+    context = get_context(request)
+    if (context['old_pass'] == User['pass']) and (context['new_pass'] == context['repeat']):
+        User['pass'] = context['new_pass']
+    return render(request, "editing_profile.html", context)
+
+def profile_edit_logo(request):
+    context = get_context(request)
+    User['logo'] = context['logo']
+    return render(request, "editing_profile.html", context)
+
+def profile_edit_email(request):
+    context = get_context(request)
+    User['email'] = context['new_email']
+    return render(request, "editing_profile.html", context)
+
+def profile_edit_username(request):
+    context = get_context(request)
+    User['name'] = context['new_name']
+    return render(request, "editing_profile.html", context)
 
 def editing_profile_page(request):
     context = get_context(request)
