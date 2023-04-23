0fe985db180d359dd213135bc6bc22ea6181f3a1
Ilya Artemyuk <mastermode1234567890@gmail.com>
2023-04-23 12:52:56 +0300

    старался как мог

--- project/urls.py
+++ project/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('profile/<int:id>', profile_views.profile_page, name="Чей-то профиль"),
     path('edit_profile', profile_views.editing_profile_page, name = "Редактирование профиля"),
     path('registration', authorization_views.registration_page, name = "Регистрация"),
-    path('authorization', authorization_views.login_page, name = "Логин"),
+    path('login', authorization_views.login_page, name = "Логин"),
     path('error404', main_views.error404_page, name = "Ошибка 404"),
     path('admin', admin_views.index_page, name="Админ. панель"),
 ]
