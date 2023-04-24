cbeb4991d52c8c8ff3c5f5ea1a0f6ff01db5be32
Andrey Bakov <andrey.bakov.2006@gmail.com>
2023-04-24 22:15:25 +0300

    clear odd files

--- main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from main.models import Servers
-
-class ServersAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Servers, ServersAdmin)
--- main/views/servers.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from main.models import Servers
-
-
-def servers_page(request):
-    context = {
-        'servers': Servers.objects.all()
-    }
-    return render(request, "servers.html", context)
