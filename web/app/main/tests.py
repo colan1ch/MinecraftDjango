d2e7f4ff06717567afe7343090e4b8471838b698
Viktor Mogilyovkin <vitya290606@gmail.com>
2023-05-14 13:03:16 +0300

    Начал писать тесты

--- web/app/main/tests.py
+++ web/app/main/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class Tests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_status_code(self):
+        response = self.client.get('', follow=False, data=None)
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_template(self):
+        response = self.client.get('', follow=False, data=None)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_servers_status_code(self):
+        self.client.login(username='fedor', password='promprom')
+        response = self.client.get('/servers', follow=True, data=None)
+        self.assertEqual(response.status_code, 200)
+
+    def test_servers_template(self):
+        self.client.login(username='fedor', password='promprom')
+        response = self.client.get('/servers', follow=True, data=None)
+        self.assertTemplateUsed(response, 'servers.html')
